0c686c52ecd50ad5e35e06bfbc19389b27a0db4e
Mina Lui <mina.lui@softwire.com>
2026-08-26 09:13:11 +0100

    EROPSPT-759: Fix vulnerabilities (#436)
    * EROPSPT-759: Fix vulnerabilities
    
    * EROPSPT-759: Upgrade internal libs
    
    * EROPSPT-759: Update suppression message
    
    * EROPSPT-759: Fix suppression message

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 import java.lang.ProcessBuilder.Redirect
 
 plugins {
-    id("org.springframework.boot") version "4.1.0"
+    id("org.springframework.boot") version "4.1.1"
     id("io.spring.dependency-management") version "1.1.7"
     kotlin("jvm") version "2.4.10"
     kotlin("kapt") version "2.4.10"
@@ -29,13 +29,10 @@ java {
     }
 }
 
-extra["awsSdkVersion"] = "2.49.4"
+extra["awsSdkVersion"] = "2.52.0"
 extra["springCloudAwsVersion"] = "4.1.0"
 // EROPSPT-733 - Pinned versions brought in by springboot - if updating springboot, check if these are still needed.
-extra["logback.version"] = "1.5.37"
-extra["netty.version"] = "4.2.16.Final"
-extra["log4j2.version"] = "2.25.5"
-extra["tomcat.version"] = "11.0.24"
+extra["tomcat.version"] = "11.0.25"
 
 allOpen {
     annotations("jakarta.persistence.Entity", "jakarta.persistence.MappedSuperclass", "jakarta.persistence.Embedabble")
@@ -58,20 +55,20 @@ repositories {
 dependencies {
     // framework
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("tools.jackson.module:jackson-module-kotlin:3.1.5")
-    implementation("tools.jackson.core:jackson-databind:3.1.5")
-    implementation("tools.jackson.core:jackson-core:3.1.5")
+    implementation("tools.jackson.module:jackson-module-kotlin:3.1.6")
+    implementation("tools.jackson.core:jackson-databind:3.1.6")
+    implementation("tools.jackson.core:jackson-core:3.1.6")
     implementation("io.github.oshai:kotlin-logging-jvm:8.0.4")
     implementation("org.apache.commons:commons-lang3:3.20.0")
     implementation("org.mapstruct:mapstruct:1.6.2")
     kapt("org.mapstruct:mapstruct-processor:1.6.2")
 
     // internal libs
-    implementation("uk.gov.dluhc:logging-library:4.2.0")
-    implementation("uk.gov.dluhc:bank-holidays-data-client-library:2.2.0")
-    implementation("uk.gov.dluhc:messaging-support-library:3.2.1")
-    implementation("uk.gov.dluhc:email-client:1.4.0")
-    implementation("uk.gov.dluhc:internal-auth-library:2.2.0")
+    implementation("uk.gov.dluhc:logging-library:4.3.0")
+    implementation("uk.gov.dluhc:bank-holidays-data-client-library:2.3.0")
+    implementation("uk.gov.dluhc:messaging-support-library:3.3.0")
+    implementation("uk.gov.dluhc:email-client:1.4.1")
+    implementation("uk.gov.dluhc:internal-auth-library:2.3.0")
 
     // api
     implementation("org.springframework.boot:spring-boot-starter-actuator")
@@ -79,6 +76,7 @@ dependencies {
     implementation("io.swagger.core.v3:swagger-annotations:2.2.7")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.integration:spring-integration-sftp")
+    implementation("org.apache.sshd:sshd-sftp:2.19.0")
     implementation("com.opencsv:opencsv:5.12.0")
 
     // Logging
@@ -145,8 +143,8 @@ dependencies {
     testImplementation("io.jsonwebtoken:jjwt-impl:0.13.0")
     testImplementation("io.jsonwebtoken:jjwt-jackson:0.13.0")
     // EROPSPT-733: Jackson v2 packages used by jjwt, should be reviewed if upgrading jjwt-jackson
-    testImplementation("com.fasterxml.jackson.core:jackson-databind:2.21.5")
-    testImplementation("com.fasterxml.jackson.core:jackson-core:2.21.5")
+    testImplementation("com.fasterxml.jackson.core:jackson-databind:2.21.6")
+    testImplementation("com.fasterxml.jackson.core:jackson-core:2.21.6")
 }
 
 kotlin {
